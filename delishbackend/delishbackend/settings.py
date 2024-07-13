40d5958ce3c0f8cf2a1c8db5517fb88eac7bbdd5
anuradhasuresh <55852292+anuradhasuresh@users.noreply.github.com>
2024-07-12 20:27:58 -0400

    Updated settings

--- delishbackend/delishbackend/settings.py
+++ delishbackend/delishbackend/settings.py
@@ -52,7 +52,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = False
 
 ROOT_URLCONF = 'delishbackend.urls'
 
