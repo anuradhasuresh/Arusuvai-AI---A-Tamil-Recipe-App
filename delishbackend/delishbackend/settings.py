71be8a47096a6ae37771ca3237b19241ea01135e
anuradhasuresh <55852292+anuradhasuresh@users.noreply.github.com>
2024-07-12 20:15:11 -0400

    Rearranged settings.py

--- delishbackend/delishbackend/settings.py
+++ delishbackend/delishbackend/settings.py
@@ -27,7 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['ec2-3-138-141-169.us-east-2.compute.amazonaws.com', 'delishdishbucket.s3-website.us-east-2.amazonaws.com']
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -44,15 +43,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'delishbackend.urls'
 
